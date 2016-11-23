ba5a405f3f2683ea3a2d386b64abe174ffbaacb9
Ketan Agnihotri <kagnihotri@tavisca.com>
2016-11-23 14:30:16 +0530

    initial commit

---
+++ OTPImplementation/Service/SmsService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Twilio;
+
+namespace OTPImplementation.Service
+{
+    public class SmsService : IIdentityMessageService
+    {
+        public Task SendAsync(IdentityMessage message)
+        {
+            // Twilio Begin
+            var Twilio = new TwilioRestClient(
+                System.Configuration.ConfigurationManager.AppSettings["SMSAccountIdentification"],
+                System.Configuration.ConfigurationManager.AppSettings["SMSAccountPassword"]);
+            var result = Twilio.SendMessage(
+                System.Configuration.ConfigurationManager.AppSettings["SMSAccountFrom"],
+                message.Destination, message.Body
+                );
+            //Status is one of Queued, Sending, Sent, Failed or null if the number is not valid
+            Trace.TraceInformation(result.Status);
+            //Twilio doesn't currently have an async API, so return success.
+            return Task.FromResult(0);
+            // Twilio End
+
+            // ASPSMS Begin 
+            // var soapSms = new MvcPWx.ASPSMSX2.ASPSMSX2SoapClient("ASPSMSX2Soap");
+            // soapSms.SendSimpleTextSMS(
+            //   System.Configuration.ConfigurationManager.AppSettings["SMSAccountIdentification"],
+            //   System.Configuration.ConfigurationManager.AppSettings["SMSAccountPassword"],
+            //   message.Destination,
+            //   System.Configuration.ConfigurationManager.AppSettings["SMSAccountFrom"],
+            //   message.Body);
+            // soapSms.Close();
+            // return Task.FromResult(0);
+            // ASPSMS End
+        }
+    }
+}
