1d919f9708861d0dc287a3405ecb93f09204b7a4
kagnihotri <kagnihotri@tavisca.com>
2016-12-02 13:44:45 +0530

    sln changed

---
+++ Dpts/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Dpts.Web;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace Dpts.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
