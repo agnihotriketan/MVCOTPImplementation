d58961182492dfc3b65be66fd262b850dfec8ed3
Ketan Agnihotri <agnihotriketan@gmail.com>
2016-12-06 14:32:47 +0530

    added working modules all

---
+++ dpts.portal.core/ExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dpts.portal.core
+{
+    public class ExceptionHandler
+    {
+        /// <summary>
+        /// Logs Exception into file
+        /// </summary>
+        /// <param name="ex">Exception</param>
+        public static void HandleException(Exception ex)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(ConfigurationManager.AppSettings["logDirectory"]);
+            file.WriteLine(ex.StackTrace);
+        }
+    }
+}
